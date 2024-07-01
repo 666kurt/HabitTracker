d00beca8ad1b991c6cdfc0cb8611da0839970d95
Maxim <incentivedie@gmail.com>
2024-07-01 22:58:04 +0400

    initial commit

---
+++ HabitTracker/App/HabitTrackerApp.swift
@@ -0,0 +1,18 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Максим Шишлов on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTracker/Modules/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by Максим Шишлов on 01.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Habit Tracker")
+    }
+}
+
+#Preview {
+    ContentView()
+}
